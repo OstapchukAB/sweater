c0b6da6f193d11eae0dbf0a375250dd45d41ad22
OstapchukAB <electron.anton@gmail.com>
2019-04-11 12:44:23 +0300

    Spring Boot: делаем простое веб приложение на Java (простой сайт)

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -1,7 +1,10 @@
 package com.example.sweater;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.youtube.com/watch?v=jH17YkBTpI4&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk
 
 @SpringBootApplication
 public class Application {
