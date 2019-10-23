18259d22a9860622d59669c39b8ad4fdaad2c3c6
OstapchukAB <electron.anton@gmail.com>
2019-10-23 15:06:55 +0300

    change Application for WAR

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -3,10 +3,28 @@ package com.example.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //https://www.youtube.com/watch?v=jH17YkBTpI4&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk
 
+
 @SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+}
+
+
+
+/*@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
@@ -14,3 +32,4 @@ public class Application {
     }
 
 }
+*/
